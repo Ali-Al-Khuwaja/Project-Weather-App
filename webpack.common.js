695433cb2ce7ad240dfb8af8d1c5180bd3a73c6d
Ali-Al-Khuwaja <132617733+Ali-Al-Khuwaja@users.noreply.github.com>
2026-05-14 19:30:41 +0300

    update file paths

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
