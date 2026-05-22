05bc808840170848c1ffebc3836cc78be67e607a
Ali-Al-Khuwaja <132617733+Ali-Al-Khuwaja@users.noreply.github.com>
2026-05-22 22:32:26 +0300

    Add api logic

--- src/api/weatherapi.js
+++ src/api/weatherapi.js
@@ -1,7 +1,11 @@
-// export async function getWeather() {
-//   const response = await fetch(
-//     'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?unitGroup=us&include=current&key=V3S4XTWZG6S9BK8V8W7FBBB3G&contentType=json'
-//   );
-//   const data = await response.json();
-//   return data;
-// }
+export async function getWeather(city) {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&include=current&key=V3S4XTWZG6S9BK8V8W7FBBB3G&contentType=json`
+  );
+  if (!response.ok) {
+    throw new Error('An error occurred when fetching the api');
+  }
+
+  const data = await response.json();
+  return data;
+}
